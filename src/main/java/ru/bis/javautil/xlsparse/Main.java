2d575a76d05d9967ee1426439b18dfa56cd4caf9
Volkov Anton <65715173+Wolf1972@users.noreply.github.com>
2026-04-22 16:49:03 +0300

    Update Main.java
    TODO for asynchronous file operations

--- src/main/java/ru/bis/javautil/xlsparse/Main.java
+++ src/main/java/ru/bis/javautil/xlsparse/Main.java
@@ -2,7 +2,10 @@ package ru.bis.javautil.xlsparse;
 
 import org.apache.commons.cli.*;
 
-public class Main {
+public class Main 
+
+    // TODO: assign tmp name to the file that will be imported 
+    // TODO: assign tmp name to the file that will be created, assign target name in the end
     public static void main(String[] args) {
         System.out.println("XLS Statement parser.");
         Util.init();
